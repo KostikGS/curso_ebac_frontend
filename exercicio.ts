90f7995aab3e92ec393dac1f06700dbe8d16cd81
KostikGS <122942355+KostikGS@users.noreply.github.com>
2023-08-22 20:42:53 -0300

    Add files via upload

---
+++ exercicio.ts
@@ -0,0 +1,16 @@
+// Função de multiplicação
+function multiplica(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+// Função de saudação
+function dizOi(name: string): string {
+    return `Olá ${name}`;
+}
+
+// Exemplo de uso das funções
+const result = multiplica(5, 3);
+console.log("Resultado da multiplicação:", result); // Saída
+
+const saudacao = dizOi("Alice");
+console.log(saudacao); // Saída
